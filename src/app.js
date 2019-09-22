7a30ea5f45a7fe55f9b6f50b4c4ab8656f789bd9
Potrac <olivier.grognuz@gmail.com>
2019-09-22 15:31:15 +0200

    added touch support and buttons

--- src/app.js
+++ src/app.js
@@ -1,18 +1,14 @@
 class Control {
 
-	/**
-	 * TODO
-	 * - add touch support
-	 * - add back button controls
-	 **/
-
-    constructor(id, title, symbol, time, container) {
+    constructor(id, time, container, title = null, symbol = null) {
         this.id = id
         this.title = title
         this.symbol = symbol
         this.time = time
         this.container = container
+    }
 
+    setupButtons() {
         this.createButton()
         this.createEffect()
     }
@@ -22,7 +18,7 @@ class Control {
     }
 
     createButton() {
-        // Create buttons links
+        // Create buttons link
         this.button = document.createElement('a')
         this.button.innerText = this.symbol
         this.button.id = this.id
@@ -48,8 +44,11 @@ class Control {
         return this.effect
     }
 
-    clickButton() {
-        this.changeAudioTime(this.time)
+    changeAudioTime() {
+        if (this.container.paused) {
+            this.container.play()
+        }
+        this.container.currentTime += this.time
 
         let _this = this
         _this.effect.classList.add('fade-in')
@@ -57,38 +56,25 @@ class Control {
             _this.effect.classList.remove('fade-in')
         }, 800)
     }
-
-    changeAudioTime(time) {
-        if (this.container.paused) {
-            this.container.play()
-        }
-        this.container.currentTime += time
-    }
-
 }
 
-class Touch {
-    constructor(id, container) {
-        this.id = id
-        this.container = container
-
+class Touch extends Control {
+    constructor(id, time, container, title, symbol) {
+        super(id, time, container, title, symbol)
         this.createTouch()
-    }
-
-    insert(referenceNode, newNode) {
-        referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+        super.createEffect()
     }
 
     createTouch() {
         this.pad = document.createElement('span')
         this.pad.classList.add('touch')
         this.pad.id = this.id
-        this.insert(this.container, this.pad)
+        super.insert(this.container, this.pad)
 
-        this.resizePads()
+        this.resizePad()
     }
 
-    resizePads() {
+    resizePad() {
         let h = this.container.offsetHeight
         let w = this.container.offsetWidth
 
@@ -103,19 +89,51 @@ class Touch {
     positionPadL() {
         this.pad.style.left = .1 * this.container.offsetWidth + 'px'
     }
+
+    changeAudioTime() {
+        super.changeAudioTime(this.time)
+    }
 }
 
-
-const PLU_audio = (typeof document.getElementsByTagName('audio')[0] == 'undefined') ? document.getElementsByTagName('video')[0] : document.getElementsByTagName('audio')[0];
+const PLU_audio = (typeof document.getElementsByTagName('audio')[0] == 'undefined') ? document.getElementsByTagName('video')[0] : document.getElementsByTagName('audio')[0]
 const PLU_parentNode = document.getElementsByClassName('video-js')[0]
 
+let buttons = []
+let but_back_10s = new Control('PLU_bck', -10, PLU_audio, '-10s', '↺')
+let but_back_5s = new Control('PLU_bck_5', -5, PLU_audio, '-5s', '↶')
+let but_forward_5s = new Control('PLU_fwd_5', 5, PLU_audio, '+5s', '↷')
+let but_forward_10s = new Control('PLU_fwd', 10, PLU_audio, '+10s', '↻')
+buttons.push(but_back_10s, but_back_5s, but_forward_5s, but_forward_10s)
+
+//Detect click event on buttons
+for (let i in buttons) {
+    buttons[i].setupButtons()
+    buttons[i].button.onclick = function (e) {
+        e = e || event
+        e.preventDefault ? e.preventDefault() : e.returnValue = false
+        buttons[i].changeAudioTime()
+    }
+}
+
 let pads = []
-let padL = new Touch('padL', PLU_audio)
-let padR = new Touch('padR', PLU_audio)
-padR.positionPadR()
-padL.positionPadL()
+let padL = new Touch('padL', -5, PLU_audio, '-5s', '↶')
+let padR = new Touch('padR', 5, PLU_audio, '+5s', '↷')
 pads.push(padL, padR)
 
+function reposition() {
+    padR.resizePad()
+    padR.positionPadR()
+    padL.resizePad()
+    padL.positionPadL()
+}
+reposition()
+
+// onresize performance enhancer
+let it
+window.onresize = function () {
+    clearTimeout(it)
+    it = setTimeout(reposition, 400)
+}
 
 for (let i in pads) {
     //Detect doubletap
@@ -130,41 +148,24 @@ for (let i in pads) {
 
         let time = (i == 0) ? -5 : 5
         if (elapsed < 500 && elapsed > 0) {
-            // pads[i].changeAudioTime(time)
-            console.log(pads[i].pad)
+            pads[i].changeAudioTime(time)
         }
         lastTap = new Date().getTime()
     })
 }
 
-let buttons = []
-let but_back_10s = new Control('PLU_bck', '-10s', '↺', -10, PLU_audio)
-let but_back_5s = new Control('PLU_bck_5', '-5s', '↶', -5, PLU_audio)
-let but_forward_5s = new Control('PLU_fwd_5', '+5s', '↷', 5, PLU_audio)
-let but_forward_10s = new Control('PLU_fwd', '+10s', '↻', 10, PLU_audio)
-buttons.push(but_back_10s, but_back_5s, but_forward_5s, but_forward_10s)
-
-//Detect click event on buttons
-for (let i in buttons) {
-    buttons[i].button.onclick = function (e) {
-        e = e || event
-        e.preventDefault ? e.preventDefault() : e.returnValue = false
-        buttons[i].clickButton()
-    }
-}
-
 //Keyboard Shortcuts
 document.onkeydown = function (e) {
     if (e.shiftKey && e.keyCode == 37) { //shift + left
-        but_back_10s.clickButton()
+        but_back_10s.changeAudioTime()
     }
     if (e.shiftKey && e.keyCode == 39) { //shift + right
-        but_forward_10s.clickButton()
+        but_forward_10s.changeAudioTime()
     }
     if (e.keyCode == '37' && !e.shiftKey) { //left
-        but_back_5s.clickButton()
+        but_back_5s.changeAudioTime()
     }
     if (e.keyCode == '39' && !e.shiftKey) { //right
-        but_forward_5s.clickButton()
+        but_forward_5s.changeAudioTime()
     }
 }
